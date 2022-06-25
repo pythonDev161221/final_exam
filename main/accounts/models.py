aac1571fdf4bbef767d353b762296c1a38b8b9f1
pythonDev161221 <ormonkenenbaev@gmail.com>
2022-06-25 10:10:51 +0600

    add models Announce and Client

--- main/accounts/models.py
+++ main/accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class Client(AbstractUser):
+    phone = models.CharField(max_length=24)
--- main/webapp/models.py
+++ main/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Announce(models.Model):
+    photo = models.ImageField()
+    title = models.CharField(max_length=128)
+    text = models.TextField(max_length=2024)
+    price = models.IntegerField(null=True, blank=True)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
+    published_at = models.DateField()
