0d65072e590a3f3ade059e451b43f63a0e474110
pythonDev161221 <ormonkenenbaev@gmail.com>
2022-06-25 18:29:25 +0600

    add delete announce

--- main/webapp/urls.py
+++ main/webapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from webapp.views import AnnounceListView, AnnounceCreateView, AnnounceUpdateView, AnnounceDetailView, AnnounceNewList, \
-    AnnounceModUpdate
+    AnnounceModUpdate, AnnounceDeleteView
 
 app_name = "webapp"
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('announce/update/<int:pk>/', AnnounceUpdateView.as_view(), name="announce_update"),
     path('announce/detail/<int:pk>/', AnnounceDetailView.as_view(), name="announce_detail"),
     path('announce/moder/list/', AnnounceNewList.as_view(), name="announce_moder_list"),
-    path('announce/moder/<int:pk>/update/', AnnounceModUpdate.as_view(), name="announce_moder_update"),
-
+    path('announce/moder/<int:pk>/update/', AnnounceModUpdate.as_view(),
+         name="announce_moder_update"),
+    path('announce/delete/<int:pk>/', AnnounceDeleteView.as_view(), name="announce_delete"),
 ]
